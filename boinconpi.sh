89b088381de14f9aec394c1613fc206aacca9e99
springfielddr <office@springfielddatarecovery.com>
2021-08-23 20:40:52 -0700

    fix apt purge

--- boinconpi.sh
+++ boinconpi.sh
@@ -199,7 +199,9 @@ echo "Updating APT..."
 apt -y update >> $LOGFILE 2>&1
 echo "Cleaning up any previous BOINC installation attempts"
 systemctl stop boinc-client >> $LOGFILE 2>&1
-apt -y purge boinc boinc-client boinc-client-opencl boinc-client-nvidia boinctui >> $LOGFILE 2>&1
+for i in boinc boinc-client boinc-client-opencl boinc-client-nvidia-cuda boinctui; do
+  apt -y purge $i >> $LOGFILE 2>&1
+done
 [ -d "/var/lib/boinc-client" ] && rm -r /var/lib/boinc-client >> $LOGFILE 2>&1
 echo "Installing BOINC.."
 for i in boinc boinc-client-opencl boinc-client-nvidia-cuda boinctui; do
